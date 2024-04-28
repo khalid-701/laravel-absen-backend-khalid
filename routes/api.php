a5c328c989b19b944b7034c510663ab38c41a3c0
khalid-701 <khalidramdhan97@gmail.com>
2024-04-29 02:29:46 +0700

    intial

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\Api\AttendanceController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CompanyController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+//login
+Route::post('/login', [AuthController::class, 'login']);
+
+//logout
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//company
+Route::get('/company', [CompanyController::class, 'show'])->middleware('auth:sanctum');
+
+// //checkin
+Route::post('/checkin', [AttendanceController::class, 'checkin'])->middleware('auth:sanctum');
+
+//checkout
+Route::post('/checkout', [AttendanceController::class, 'checkout'])->middleware('auth:sanctum');
+
+//is checkin
+Route::get('/is-checkin', [AttendanceController::class, 'isCheckedin'])->middleware('auth:sanctum');
+
+//update profile
+Route::post('/update-profile', [AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
+
+//create permission
+Route::apiResource('/api-permissions', App\Http\Controllers\Api\PermissionController::class)->middleware('auth:sanctum');
+//notes
+Route::apiResource('/api-notes', App\Http\Controllers\Api\NoteController::class)->middleware('auth:sanctum');
